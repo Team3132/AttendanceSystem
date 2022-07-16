2f0d7b765b2d25f924eb6c02a4eb3ad7053f5b7c
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-07-16 16:08:36 +1000

    export without loadable

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,4 +2,4 @@ import loadable from "@loadable/component";
 
 export * from "./actions";
 export * from "./randomString";
-export const theme = loadable.lib(() => import("./theme"));
+export * from "./theme";
