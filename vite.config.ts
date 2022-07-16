60f99f3fc4f651e9fe860614cf16dd0ca5fd3e6f
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-07-16 15:17:04 +1000

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tsconfigPaths(),
     react(),
+    VitePWA({ registerType: "autoUpdate", injectRegister: "auto" }),
     visualizer({
       template: "treemap", // "sunburst" | "treemap" | "network"
     }),
