3e1a7c3219453ce3ffe4cb923ff8e47bcaae0be5
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2022-08-13 10:37:49 +1000

    add fetcher

--- src/hooks/fetcher.ts
+++ src/hooks/fetcher.ts
@@ -1,5 +1,10 @@
-import axios from "axios";
+import axios, { Axios } from "axios";
 import { BareFetcher } from "swr";
 
+const api = axios.create({
+  baseURL: "https://api.sebasptsch.dev",
+  withCredentials: true,
+});
+
 export const fetcher: BareFetcher<any> = (url: string, params?: any) =>
-  axios.get(url, { params }).then((res) => res.data);
+  api.get(url, { params }).then((res) => res.data);
