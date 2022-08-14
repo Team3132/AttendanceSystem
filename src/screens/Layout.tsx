421801b130aff3bb30f3d80cc545774f58f26019
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2022-08-14 13:36:27 +1000

    add props to nav

--- src/screens/Layout.tsx
+++ src/screens/Layout.tsx
@@ -1,12 +1,14 @@
+import { useAuthStatus } from "@/hooks";
 import { Container, Flex } from "@chakra-ui/react";
 import { Nav } from "@components";
 import * as React from "react";
 import { Outlet } from "react-router-dom";
 
 export const Layout: React.FC = () => {
+  const { isAdmin, isAuthenticated } = useAuthStatus();
   return (
     <Flex direction={"column"} minH="100vh">
-      <Nav />
+      <Nav isAdmin={!!isAdmin} isAuthenticated={!!isAuthenticated} />
       <Container maxW="container.lg" height="100%">
         <Outlet />
       </Container>
