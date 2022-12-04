1ed6c4e97e1a1487a6b1efd339d4e8dc9a3a21c4
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-12-04 12:00:42 +0000

    fix weird key

--- src/features/user/hooks/keys.ts
+++ src/features/user/hooks/keys.ts
@@ -1,7 +1,7 @@
 export const userKeys = {
   all: ["users"] as const,
   users: () => [...userKeys.all, "users"] as const,
-  user: (id: string) => [...userKeys.users(), id] as const,
+  user: (id: string) => [...userKeys.all, id] as const,
   avatars: () => [...userKeys.all, "avatars"] as const,
   avatar: (id: string) => [...userKeys.avatars(), id] as const,
   outreachReports: () => [...userKeys.all, "outreachReports"] as const,
