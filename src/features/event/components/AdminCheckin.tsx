e7a01210c7e1606a6c4ec8ed0cba3fab62f1ef6b
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2023-01-07 06:15:11 +0000

    full screen button

--- src/features/event/components/AdminCheckin.tsx
+++ src/features/event/components/AdminCheckin.tsx
@@ -1,6 +1,8 @@
 import { ScancodeInput } from "@/features/scancode";
 import { EventResponseType } from "@/generated";
-import { Stack, StatGroup } from "@chakra-ui/react";
+import { IconButton, Stack, StatGroup } from "@chakra-ui/react";
+import { useRef } from "react";
+import { MdFullscreen } from "react-icons/md";
 import EventTotpStat from "./EventTotpStat";
 
 interface AdminCheckinProps {
@@ -9,9 +11,23 @@ interface AdminCheckinProps {
 
 export default function AdminCheckin(props: AdminCheckinProps) {
   const { event } = props;
+  const adminScreenRef = useRef<HTMLDivElement | null>(null);
+
+  const handleFullscreen: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+    adminScreenRef.current?.requestFullscreen();
+  };
 
   return (
-    <Stack alignContent={"center"}>
+    <Stack alignContent={"center"}  position="relative" bgColor={"chakra-body-bg"} ref={adminScreenRef} p={5}>
+      <IconButton
+        icon={<MdFullscreen />}
+        position="absolute"
+        right={5}
+        bottom="auto"
+        top={5}
+        aria-label={"Fullscreen"}
+        onClick={handleFullscreen}
+      />
       <StatGroup p={5} textAlign="center">
         <EventTotpStat eventId={event.id} />
       </StatGroup>
