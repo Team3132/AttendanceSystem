242918170763e809403fa3526340a2d556465b31
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-01-18 20:31:31 +1100

    Improved Table

--- src/features/rsvp/components/RSVPList.tsx
+++ src/features/rsvp/components/RSVPList.tsx
@@ -9,35 +9,67 @@ import useEventRSVPStatuses from "../hooks/useEventRSVPStatuses";
 const columnHelper = createColumnHelper<RsvpUser>();
 
 const columns = [
-  columnHelper.accessor("user.firstName", {
-    header: "First Name",
-    footer: "First Name",
+  columnHelper.group({
+    id: "name",
+    header: "Name",
+    columns: [
+      columnHelper.accessor("user.firstName", {
+        header: "First Name",
+        // footer: "First Name",
+      }),
+      columnHelper.accessor("user.lastName", {
+        header: "Last Name",
+        // footer: "Last Name",
+      }),
+    ],
   }),
-  columnHelper.accessor("user.lastName", {
-    header: "Last Name",
-    footer: "Last Name",
-  }),
-  columnHelper.accessor("status", {
+  columnHelper.group({
     id: "status",
     header: "Status",
-    footer: "Status",
-    cell: (props) => {
-      return props.getValue()?.toString() ?? "Unknown";
-    },
+    columns: [
+      columnHelper.accessor("status", {
+        id: "status",
+        header: "RSVP Status",
+        // footer: "RSVP Status",
+        cell: ({ getValue }) => {
+          return readableStatus(getValue<RsvpUser.status>());
+        },
+      }),
+      columnHelper.accessor("attended", {
+        id: "attended",
+        header: "Attended",
+        // footer: "Attended",
+        cell: (props) => (props.getValue() ? "Yes" : "No"),
+      }),
+    ],
   }),
-  columnHelper.accessor("attended", {
-    id: "attended",
-    header: "Attended",
-    footer: "Attended",
-    cell: (props) => (props.getValue() ? "Yes" : "No"),
+  columnHelper.group({
+    id: "meta",
+    columns: [
+      columnHelper.accessor("updatedAt", {
+        id: "updatedAt",
+        header: "Updated At",
+        // footer: "Updated At",
+        cell: (props) =>
+          DateTime.fromISO(props.getValue()).toLocaleString(
+            DateTime.DATETIME_MED
+          ),
+      }),
+    ],
   }),
-  columnHelper.accessor("updatedAt", {
-    id: "updatedAt",
-    header: "Updated At",
-    footer: "Updated At",
-    cell: (props) => DateTime.fromISO(props.getValue()).toLocaleString(DateTime.DATETIME_MED)
-  })
 ];
+
+const readableStatus = (status: RsvpUser.status | null) => {
+  if (status === null) {
+    return "Unknown";
+  } else if (status === "YES") {
+    return "Coming";
+  } else if (status === "MAYBE") {
+    return "Maybe";
+  } else {
+    return "Not Coming";
+  }
+};
 
 export interface RSVPListProps {
   eventId?: string;
