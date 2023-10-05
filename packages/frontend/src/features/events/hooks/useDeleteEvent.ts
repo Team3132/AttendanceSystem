1d9796519dc6d1747cf6c41f812470ed16564d2f
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2023-10-05 11:05:33 +1100

    fix(frontend): Navigate away from event when deleted

--- packages/frontend/src/features/events/hooks/useDeleteEvent.ts
+++ packages/frontend/src/features/events/hooks/useDeleteEvent.ts
@@ -1,9 +1,11 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import api from "../../../api";
 import { eventKeys } from "../../../api/query/event.api";
+import { useNavigate } from "react-router-dom";
 
 export default function useDeleteEvent() {
   const queryClient = useQueryClient();
+  const navigate = useNavigate();
   return useMutation({
     mutationFn: async (eventId: string) => {
       api.event.deleteEvent(eventId);
@@ -15,6 +17,7 @@ export default function useDeleteEvent() {
       queryClient.removeQueries({
         queryKey: eventKeys.event(eventId),
       });
+      navigate("/events");
     },
   });
 }
