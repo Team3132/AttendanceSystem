4353e37ae0563c97e9b96be5f6df4cfaf8ae571f
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2023-12-24 18:11:49 +1100

    fix(bot): Discard seconds

--- packages/bot/src/bot/buttons/OutreachPaginationButton.ts
+++ packages/bot/src/bot/buttons/OutreachPaginationButton.ts
@@ -25,8 +25,7 @@ const roundDuration = (duration: Duration) => {
   const rounded = Math.round(millis / 60000) * 60000;
   const hours = Math.floor(rounded / 3600000);
   const minutes = Math.floor((rounded % 3600000) / 60000);
-  const seconds = Math.floor((rounded % 60000) / 1000);
-  return Duration.fromObject({ hours, minutes, seconds });
+  return Duration.fromObject({ hours, minutes });
 };
 
 const leaderboardLine = (data: z.infer<typeof LeaderBoardUser>) =>
