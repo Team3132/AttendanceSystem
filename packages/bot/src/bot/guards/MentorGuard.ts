54599ef73272f0adf0cbe0804b9b3d54a2d15882
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2024-01-06 18:09:00 +1100

    add mentor guard

---
+++ packages/bot/src/bot/guards/MentorGuard.ts
@@ -0,0 +1,62 @@
+import { BACKEND_TOKEN, type BackendClient } from '@/backend/backend.module';
+import { ROLES } from '@/constants';
+import {
+  CanActivate,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { GuildMember } from 'discord.js';
+import { NecordExecutionContext } from 'necord';
+
+@Injectable()
+export class MentorGuard implements CanActivate {
+  constructor(
+    @Inject(BACKEND_TOKEN) private readonly backendClient: BackendClient,
+    private readonly config: ConfigService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const logger = new Logger(MentorGuard.name);
+    try {
+      const [interaction] =
+        NecordExecutionContext.create(
+          context,
+        ).getContext<'interactionCreate'>();
+
+      const approvedGuildId = this.config.getOrThrow('GUILD_ID');
+      const guildId = interaction.guildId;
+
+      if (!guildId || guildId !== approvedGuildId) return false;
+
+      const guildMember = interaction.member;
+
+      if (!(guildMember instanceof GuildMember)) return false;
+
+      const fetchedUser = await guildMember.fetch();
+
+      const userRoles = [
+        ...fetchedUser.roles.cache.mapValues((role) => role.id).values(),
+      ];
+
+      const username = fetchedUser.nickname ?? fetchedUser.user.username;
+
+      const user = await this.backendClient.client.bot.findOrCreateUser.mutate({
+        id: fetchedUser.id,
+        username,
+        roles: userRoles,
+      });
+
+      logger.log(`User ${fetchedUser.id} (${username}) has been updated`);
+
+      if (user.roles?.includes(ROLES.MENTOR)) return true;
+
+      return false;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+}
