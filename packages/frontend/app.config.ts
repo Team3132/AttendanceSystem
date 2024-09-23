cffe9917201bdbd01f5ad4c9f8ab8b7071306176
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2024-09-23 07:04:50 +0000

    renaming

--- packages/frontend/app.config.ts
+++ packages/frontend/app.config.ts
@@ -3,10 +3,6 @@ import { defineConfig } from "@tanstack/start/config";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  tsr: {
-    appDirectory: "src",
-    routesDirectory: "src/pages",
-  },
   vite: {
     plugins: () => [
       // this is the plugin that enables path aliases
