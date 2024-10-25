3a16123907c437a734126ea5c3ed4d0fafd4715a
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2024-10-25 11:11:40 +1100

    messing about

--- packages/frontend/app/server/auth/authGuard.ts
+++ packages/frontend/app/server/auth/authGuard.ts
@@ -1,80 +1,79 @@
-"use server";
+'use server';
 
-import { setCookie, getCookie } from "vinxi/http";
-import { lucia } from "./lucia";
+import { setCookie, getCookie } from 'vinxi/http';
+import { lucia } from './lucia';
 import {
   redirect,
   RegisteredRouter,
   ToPathOption,
-} from "@tanstack/react-router";
-import { createServerFn } from "@tanstack/start";
+} from '@tanstack/react-router';
+import { createServerFn } from '@tanstack/start';
 
 interface AuthGuardParams {
   failureRedirect?: ToPathOption<RegisteredRouter>;
   successRedirect?: ToPathOption<RegisteredRouter>;
 }
 
-export const authGuard = createServerFn(
-  "GET",
-  async (params: AuthGuardParams, ctx) => {
-    "use server";
-    try {
-      console.log({ ctx });
-      console.log("authGuard");
-      const { failureRedirect, successRedirect } = params;
+const serverFn = async (params: AuthGuardParams, ctx) => {
+  'use server';
+  try {
+    console.log({ ctx });
+    console.log('authGuard');
+    const { failureRedirect, successRedirect } = params;
 
-      const sessionId = getCookie(lucia.sessionCookieName);
+    const sessionId = getCookie(lucia.sessionCookieName);
 
-      if (!sessionId) {
-        if (failureRedirect) {
-          throw redirect({
-            to: failureRedirect,
-            statusCode: 302,
-          });
-        }
-
-        return { session: null, user: null };
-      }
-
-      const { session, user } = await lucia.validateSession(sessionId);
-      if (!session) {
-        const sessionCookie = lucia.createBlankSessionCookie();
-        setCookie(
-          sessionCookie.name,
-          sessionCookie.value,
-          sessionCookie.attributes,
-        );
-
-        if (failureRedirect) {
-          throw redirect({
-            to: failureRedirect ?? "/",
-            statusCode: 302,
-          });
-        }
-
-        return { session: null, user: null };
-      }
-
-      if (session?.fresh) {
-        const sessionCookie = lucia.createSessionCookie(session.id);
-        setCookie(
-          sessionCookie.name,
-          sessionCookie.value,
-          sessionCookie.attributes,
-        );
-      }
-
-      if (successRedirect) {
+    if (!sessionId) {
+      if (failureRedirect) {
         throw redirect({
-          to: successRedirect,
+          to: failureRedirect,
           statusCode: 302,
         });
       }
 
-      return { session, user };
-    } catch (error) {
-      console.error(error);
-      throw error;
+      return { session: null, user: null };
     }
-  },
-);
+
+    const { session, user } = await lucia.validateSession(sessionId);
+    if (!session) {
+      const sessionCookie = lucia.createBlankSessionCookie();
+      setCookie(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes,
+      );
+
+      if (failureRedirect) {
+        throw redirect({
+          to: failureRedirect ?? '/',
+          statusCode: 302,
+        });
+      }
+
+      return { session: null, user: null };
+    }
+
+    if (session?.fresh) {
+      const sessionCookie = lucia.createSessionCookie(session.id);
+      setCookie(
+        sessionCookie.name,
+        sessionCookie.value,
+        sessionCookie.attributes,
+      );
+    }
+
+    if (successRedirect) {
+      throw redirect({
+        to: successRedirect,
+        statusCode: 302,
+      });
+    }
+
+    return { session, user };
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+export const authGuard = createServerFn('GET', serverFn);
