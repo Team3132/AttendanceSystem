c21ca87a2076bca1aafa0a630175c5ea2d9a3a15
Sebastian Pietschner <32213671+sebasptsch@users.noreply.github.com>
2024-11-10 11:47:05 +1100

    Pwa (#2032)

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +14,22 @@ export default defineConfig({
     viteTsconfigPaths({
       projects: ["./tsconfig.json"],
     }),
-    // mkcert()
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        navigateFallbackDenylist: [/^\/api/],
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => url.pathname.startsWith("/api"),
+            handler: "NetworkOnly",
+            options: {
+              cacheName: "api-cache",
+            },
+          },
+        ],
+      },
+    }),
   ],
 
   clearScreen: false,
