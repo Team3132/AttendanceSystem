e342af2bdc393c797859dddf8f87856d012b5a48
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2024-11-10 00:34:14 +0000

    I loooove PWA

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +16,17 @@ export default defineConfig({
     }),
     VitePWA({
       registerType: "autoUpdate",
-    })
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+        navigateFallbackDenylist: [/^\/api/],
+        runtimeCaching: [
+          {
+            urlPattern: ({ url }) => url.pathname.startsWith("/api"),
+            handler: "NetworkOnly",
+          },
+        ],
+      },
+    }),
   ],
 
   clearScreen: false,
