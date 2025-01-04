6ff929c8677be7a2d47e926ce892f273e2ae03af
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2025-01-05 09:46:06 +1100

    better labels

--- packages/frontend/app/features/events/components/AdminRsvpListItem.tsx
+++ packages/frontend/app/features/events/components/AdminRsvpListItem.tsx
@@ -15,68 +15,94 @@ import { FaCheck, FaClock, FaGear, FaQuestion, FaXmark } from "react-icons/fa6";
 import type { z } from "zod";
 import { useDisclosure } from "../../../hooks/useDisclosure";
 import { MdEdit } from "react-icons/md";
-import { useCallback, useId, useRef } from "react";
+import { useCallback, useId, useMemo, useRef } from "react";
 import useAddUserRsvp from "../hooks/useAddRsvp";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { eventQueryOptions } from "@/queries/events.queries";
 import { parseDate } from "@/utils/date";
 import RSVPActionsButton from "./RSVPActionsButton";
+import { check } from "drizzle-orm/pg-core";
 
 interface AdminRSVPListItemProps {
   rsvp: z.infer<typeof RSVPUserSchema>;
 }
 
 export default function AdminRSVPListItem({ rsvp }: AdminRSVPListItemProps) {
+  const { isSameDay, checkout, checkin } = useMemo(() => {
+    const checkinIso = parseDate(rsvp.checkinTime);
+    const checkoutIso = parseDate(rsvp.checkoutTime);
+
+    const checkin = checkinIso ? DateTime.fromISO(checkinIso) : null;
+    const checkout = checkoutIso ? DateTime.fromISO(checkoutIso) : null;
+    const isSameDay =
+      checkin && checkout ? checkin.hasSame(checkout, "day") : false;
+
+    return {
+      checkin,
+      checkout,
+      isSameDay,
+    };
+  }, [rsvp.checkinTime, rsvp.checkoutTime]);
+
+  const message = useMemo(() => {
+    if (isSameDay && checkin && checkout) {
+      // If the checkin and checkout are on the same day, show the time range
+      return `${checkin.toLocaleString(
+        DateTime.TIME_SIMPLE,
+      )} - ${checkout.toLocaleString(DateTime.TIME_SIMPLE)}`;
+    }
+
+    if (checkin && checkout) {
+      // If the checkin and checkout are not on the same day, show the date range
+      return `${checkin.toLocaleString(
+        DateTime.DATETIME_MED,
+      )} - ${checkout.toLocaleString(DateTime.DATETIME_MED)}`;
+    }
+
+    if (checkin) {
+      // If there is a checkin time, show the checkin time
+      return `Checked in at ${checkin.toLocaleString(DateTime.TIME_SIMPLE)}`;
+    }
+
+    // If there is no checkin time, show that there is no checkin
+    return "No check-in";
+  }, [isSameDay, checkin, checkout]);
+
   return (
-    <>
-      <ListItem
-        secondaryAction={
-          <RSVPActionsButton eventId={rsvp.eventId} userId={rsvp.userId} />
-        }
-      >
-        <Tooltip title={rsvp.status ?? "No response"}>
-          <ListItemAvatar>
-            <Avatar
-              sx={{
-                bgcolor:
-                  rsvp.status === "YES" || rsvp.status === "ATTENDED"
-                    ? "success.main"
-                    : rsvp.status === "NO"
-                      ? "error.main"
-                      : rsvp.status === "LATE"
-                        ? "warning.main"
-                        : undefined,
-              }}
-            >
-              {rsvp.status === null ? (
-                ""
-              ) : rsvp.status === "YES" || rsvp.status === "ATTENDED" ? (
-                <FaCheck />
-              ) : rsvp.status === "NO" ? (
-                <FaXmark />
-              ) : rsvp.status === "LATE" ? (
-                <FaClock />
-              ) : (
-                <FaQuestion />
-              )}
-            </Avatar>
-          </ListItemAvatar>
-        </Tooltip>
-        <ListItemText
-          primary={rsvp.user.username}
-          secondary={
-            rsvp.checkinTime && rsvp.checkoutTime && rsvp.status === "ATTENDED"
-              ? `Checked out at ${DateTime.fromMillis(
-                  Date.parse(rsvp.checkoutTime),
-                ).toLocaleString(DateTime.TIME_SIMPLE)}`
-              : rsvp.checkinTime
-                ? `Checked in at ${DateTime.fromMillis(
-                    Date.parse(rsvp.checkinTime),
-                  ).toLocaleString(DateTime.TIME_SIMPLE)}`
-                : "No check-in"
-          }
-        />
-      </ListItem>
-    </>
+    <ListItem
+      secondaryAction={
+        <RSVPActionsButton eventId={rsvp.eventId} userId={rsvp.userId} />
+      }
+    >
+      <Tooltip title={rsvp.status ?? "No response"}>
+        <ListItemAvatar>
+          <Avatar
+            sx={{
+              bgcolor:
+                rsvp.status === "YES" || rsvp.status === "ATTENDED"
+                  ? "success.main"
+                  : rsvp.status === "NO"
+                    ? "error.main"
+                    : rsvp.status === "LATE"
+                      ? "warning.main"
+                      : undefined,
+            }}
+          >
+            {rsvp.status === null ? (
+              ""
+            ) : rsvp.status === "YES" || rsvp.status === "ATTENDED" ? (
+              <FaCheck />
+            ) : rsvp.status === "NO" ? (
+              <FaXmark />
+            ) : rsvp.status === "LATE" ? (
+              <FaClock />
+            ) : (
+              <FaQuestion />
+            )}
+          </Avatar>
+        </ListItemAvatar>
+      </Tooltip>
+      <ListItemText primary={rsvp.user.username} secondary={message} />
+    </ListItem>
   );
 }
