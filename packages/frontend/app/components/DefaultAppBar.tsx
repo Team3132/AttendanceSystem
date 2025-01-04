2c043677c2ecb639b7ff746d02a6203bf02fe9a8
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2025-01-04 21:47:18 +1100

    fix page wrapper

--- packages/frontend/app/components/DefaultAppBar.tsx
+++ packages/frontend/app/components/DefaultAppBar.tsx
@@ -9,30 +9,20 @@ interface DefaultAppBarProps {
 
 export default function DefaultAppBar({ title }: DefaultAppBarProps) {
   return (
-    <>
-      <AppBar
-        position="absolute"
-        sx={{
-          top: 0,
-          left: 0,
-          right: 0,
-        }}
-      >
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            {title}
-          </Typography>
-          <Stack direction="row" spacing={1}>
-            <ModeSwitchButton />
-            <AsChildLink to="/profile">
-              <IconButton color="inherit">
-                <FaCircleUser />
-              </IconButton>
-            </AsChildLink>
-          </Stack>
-        </Toolbar>
-      </AppBar>
-      <Toolbar />
-    </>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          {title}
+        </Typography>
+        <Stack direction="row" spacing={1}>
+          <ModeSwitchButton />
+          <AsChildLink to="/profile">
+            <IconButton color="inherit">
+              <FaCircleUser />
+            </IconButton>
+          </AsChildLink>
+        </Stack>
+      </Toolbar>
+    </AppBar>
   );
 }
