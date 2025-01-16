511da10ba02702a2bdd238228cd3e5314486c45f
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2025-01-17 09:36:04 +1100

    fix admin table

--- packages/frontend/app/components/DataTable.tsx
+++ packages/frontend/app/components/DataTable.tsx
@@ -1,238 +1,239 @@
 import {
-	Paper,
-	Table,
-	TableBody,
-	TableCell,
-	TableContainer,
-	type TableContainerProps,
-	TableHead,
-	type TableProps,
-	TableRow,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  type TableContainerProps,
+  TableHead,
+  type TableProps,
+  TableRow,
 } from "@mui/material";
 import { type RankingInfo, rankItem } from "@tanstack/match-sorter-utils";
 import {
-	type ColumnDef,
-	type FilterFn,
-	type OnChangeFn,
-	type Row,
-	type RowSelectionState,
-	type SortingState,
-	type VisibilityState,
-	flexRender,
-	getCoreRowModel,
-	getFilteredRowModel,
-	getSortedRowModel,
-	useReactTable,
+  type ColumnDef,
+  type FilterFn,
+  type OnChangeFn,
+  type Row,
+  RowData,
+  type RowSelectionState,
+  type SortingState,
+  type VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  useReactTable,
 } from "@tanstack/react-table";
 import { useVirtualizer } from "@tanstack/react-virtual";
 import React, { useMemo, useRef, useState } from "react";
 import { FaArrowDown, FaArrowUp } from "react-icons/fa6";
 
 export interface DatatableProps<Data extends object>
-	extends TableContainerProps {
-	data: Data[];
-	// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-	columns: ColumnDef<Data, any>[];
-	globalFilter?: string;
-	setGlobalFilter?: (filter: string) => void;
-	size?: TableProps["size"];
-	enableRowSelection?: "single" | "multiple";
-	rowSelection?: RowSelectionState;
-	onRowSelectionChange?: OnChangeFn<RowSelectionState>;
-	fetchNextPage?: () => void;
-	isFetching?: boolean;
-	totalDBRowCount?: number;
-	fixedHeight?: number;
-	onRowSelect?: (row: Row<Data>) => void;
+  extends TableContainerProps {
+  data: Data[];
+  // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+  columns: ColumnDef<Data, any>[];
+  globalFilter?: string;
+  setGlobalFilter?: (filter: string) => void;
+  size?: TableProps["size"];
+  enableRowSelection?: "single" | "multiple";
+  rowSelection?: RowSelectionState;
+  onRowSelectionChange?: OnChangeFn<RowSelectionState>;
+  fetchNextPage?: () => void;
+  isFetching?: boolean;
+  totalDBRowCount?: number;
+  fixedHeight?: number;
+  onRowSelect?: (row: Row<Data>) => void;
 }
 
 declare module "@tanstack/table-core" {
-	interface FilterFns {
-		// biome-ignore lint/suspicious/noExplicitAny: <explanation>
-		fuzzy: FilterFn<any>;
-	}
-	interface FilterMeta {
-		itemRank: RankingInfo;
-	}
+  interface FilterFns {
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    fuzzy: FilterFn<any>;
+  }
+  interface FilterMeta {
+    itemRank: RankingInfo;
+  }
 }
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 const fuzzyFilter: FilterFn<any> = (row, columnId, value, addMeta) => {
-	// Rank the item
-	const itemRank = rankItem(row.getValue(columnId), value);
+  // Rank the item
+  const itemRank = rankItem(row.getValue(columnId), value);
 
-	// Store the itemRank info
-	addMeta({
-		itemRank,
-	});
+  // Store the itemRank info
+  addMeta({
+    itemRank,
+  });
 
-	// Return if the item should be filtered in/out
-	return itemRank.passed;
+  // Return if the item should be filtered in/out
+  return itemRank.passed;
 };
 
 export default function Datatable<Data extends object>({
-	data,
-	columns,
-	globalFilter,
-	setGlobalFilter,
-	size,
-	rowSelection,
-	onRowSelectionChange,
-	enableRowSelection,
-	fetchNextPage,
-	fixedHeight,
-	isFetching,
-	totalDBRowCount,
-	...tableContainerProps
+  data,
+  columns,
+  globalFilter,
+  setGlobalFilter,
+  size,
+  rowSelection,
+  onRowSelectionChange,
+  enableRowSelection,
+  fetchNextPage,
+  fixedHeight,
+  isFetching,
+  totalDBRowCount,
+  ...tableContainerProps
 }: DatatableProps<Data>) {
-	const [sorting, setSorting] = useState<SortingState>([]);
-	const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
 
-	const table = useReactTable({
-		columns,
-		data,
-		getCoreRowModel: getCoreRowModel(),
-		getFilteredRowModel: getFilteredRowModel(),
-		onSortingChange: setSorting,
-		onColumnVisibilityChange: setColumnVisibility,
-		getSortedRowModel: getSortedRowModel(),
-		onGlobalFilterChange: setGlobalFilter,
-		onRowSelectionChange,
-		enableRowSelection: enableRowSelection === "multiple",
-		enableMultiRowSelection: enableRowSelection === "multiple",
-		enableGlobalFilter: true,
-		filterFns: {
-			fuzzy: fuzzyFilter,
-		},
-		globalFilterFn: fuzzyFilter,
-		state: {
-			sorting,
-			columnVisibility,
-			globalFilter,
-			rowSelection,
-		},
-	});
+  const table = useReactTable<Data>({
+    columns,
+    data,
+    getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onSortingChange: setSorting,
+    onColumnVisibilityChange: setColumnVisibility,
+    getSortedRowModel: getSortedRowModel(),
+    onGlobalFilterChange: setGlobalFilter,
+    onRowSelectionChange,
+    enableRowSelection: enableRowSelection === "multiple",
+    enableMultiRowSelection: enableRowSelection === "multiple",
+    enableGlobalFilter: true,
+    filterFns: {
+      fuzzy: fuzzyFilter,
+    },
+    globalFilterFn: fuzzyFilter,
+    state: {
+      sorting,
+      columnVisibility,
+      globalFilter,
+      rowSelection,
+    },
+  });
 
-	const tableContainerRef = useRef<HTMLDivElement>(null);
+  const tableContainerRef = useRef<HTMLDivElement>(null);
 
-	const { rows } = table.getRowModel();
-	const virtualizer = useVirtualizer({
-		getScrollElement: () => tableContainerRef.current,
-		count: rows.length,
-		estimateSize: () => fixedHeight || 48,
-		overscan: 10,
-	});
+  const { rows } = table.getRowModel();
+  const virtualizer = useVirtualizer({
+    getScrollElement: () => tableContainerRef.current,
+    count: rows.length,
+    estimateSize: () => fixedHeight || 48,
+    overscan: 10,
+  });
 
-	const totalFetched = useMemo(() => {
-		return rows.length;
-	}, [rows]);
+  const totalFetched = useMemo(() => {
+    return rows.length;
+  }, [rows]);
 
-	const fetchMoreOnBottomReached = React.useCallback(
-		(containerRefElement?: HTMLDivElement | null) => {
-			if (
-				containerRefElement &&
-				fetchNextPage &&
-				totalDBRowCount !== undefined
-			) {
-				const { scrollHeight, scrollTop, clientHeight } = containerRefElement;
-				//once the user has scrolled within 300px of the bottom of the table, fetch more data if there is any
-				if (
-					scrollHeight - scrollTop - clientHeight < 300 &&
-					!isFetching &&
-					totalFetched < totalDBRowCount
-				) {
-					fetchNextPage();
-				}
-			}
-		},
-		[fetchNextPage, isFetching, totalFetched, totalDBRowCount],
-	);
+  const fetchMoreOnBottomReached = React.useCallback(
+    (containerRefElement?: HTMLDivElement | null) => {
+      if (
+        containerRefElement &&
+        fetchNextPage &&
+        totalDBRowCount !== undefined
+      ) {
+        const { scrollHeight, scrollTop, clientHeight } = containerRefElement;
+        //once the user has scrolled within 300px of the bottom of the table, fetch more data if there is any
+        if (
+          scrollHeight - scrollTop - clientHeight < 300 &&
+          !isFetching &&
+          totalFetched < totalDBRowCount
+        ) {
+          fetchNextPage();
+        }
+      }
+    },
+    [fetchNextPage, isFetching, totalFetched, totalDBRowCount]
+  );
 
-	React.useEffect(() => {
-		fetchMoreOnBottomReached(tableContainerRef.current);
-	}, [fetchMoreOnBottomReached]);
+  React.useEffect(() => {
+    fetchMoreOnBottomReached(tableContainerRef.current);
+  }, [fetchMoreOnBottomReached]);
 
-	return (
-		<TableContainer
-			component={Paper}
-			ref={tableContainerRef}
-			// biome-ignore lint/suspicious/noExplicitAny: Weird type intersection if there's no any
-			onScroll={(e: any) =>
-				fetchMoreOnBottomReached(e.target as HTMLDivElement)
-			}
-			{...tableContainerProps}
-		>
-			<div style={{ height: `${virtualizer.getTotalSize()}px` }}>
-				<Table stickyHeader size={size}>
-					<TableHead>
-						{table.getHeaderGroups().map((headerGroup) => (
-							<TableRow key={headerGroup.id}>
-								{headerGroup.headers.map((header) => (
-									<TableCell
-										component={"th"}
-										key={header.id}
-										colSpan={header.colSpan}
-										style={{ width: header.getSize() }}
-									>
-										{header.isPlaceholder ? null : (
-											// biome-ignore lint/a11y/useKeyWithClickEvents: I'm not sure what this is
-											<div onClick={header.column.getToggleSortingHandler()}>
-												<b>
-													{flexRender(
-														header.column.columnDef.header,
-														header.getContext(),
-													)}
-													{{
-														asc: <FaArrowUp fontSize={"small"} />,
-														desc: <FaArrowDown fontSize={"small"} />,
-													}[header.column.getIsSorted() as string] ?? null}
-												</b>
-											</div>
-										)}
-									</TableCell>
-								))}
-							</TableRow>
-						))}
-					</TableHead>
+  return (
+    <TableContainer
+      component={Paper}
+      ref={tableContainerRef}
+      // biome-ignore lint/suspicious/noExplicitAny: Weird type intersection if there's no any
+      onScroll={(e: any) =>
+        fetchMoreOnBottomReached(e.target as HTMLDivElement)
+      }
+      {...tableContainerProps}
+    >
+      <div style={{ height: `${virtualizer.getTotalSize()}px` }}>
+        <Table stickyHeader size={size}>
+          <TableHead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <TableCell
+                    component={"th"}
+                    key={header.id}
+                    colSpan={header.colSpan}
+                    style={{ width: header.getSize() }}
+                  >
+                    {header.isPlaceholder ? null : (
+                      // biome-ignore lint/a11y/useKeyWithClickEvents: I'm not sure what this is
+                      <div onClick={header.column.getToggleSortingHandler()}>
+                        <b>
+                          {flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                          {{
+                            asc: <FaArrowUp fontSize={"small"} />,
+                            desc: <FaArrowDown fontSize={"small"} />,
+                          }[header.column.getIsSorted() as string] ?? null}
+                        </b>
+                      </div>
+                    )}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
+          </TableHead>
 
-					<TableBody>
-						{virtualizer.getVirtualItems().map((virtualRow, index) => {
-							const row = rows[virtualRow.index] as Row<Data>;
-							return (
-								<TableRow
-									key={row.id}
-									ref={(node) => virtualizer.measureElement(node)}
-									component="tr"
-									data-index={virtualRow.index}
-									style={{
-										transform: `translateY(${
-											virtualRow.start - index * virtualRow.size
-										}px)`,
-									}}
-								>
-									{row.getVisibleCells().map((cell) => {
-										//   const meta: any = cell.column.columnDef.meta;
-										return (
-											<TableCell
-												key={cell.id}
-												component="td"
-												style={{
-													width: cell.column.getSize(),
-												}}
-											>
-												{flexRender(
-													cell.column.columnDef.cell,
-													cell.getContext(),
-												)}
-											</TableCell>
-										);
-									})}
-								</TableRow>
-							);
-						})}
-					</TableBody>
-				</Table>
-			</div>
-		</TableContainer>
-	);
+          <TableBody>
+            {virtualizer.getVirtualItems().map((virtualRow, index) => {
+              const row = rows[virtualRow.index] as Row<Data>;
+              return (
+                <TableRow
+                  key={row.id}
+                  ref={(node) => virtualizer.measureElement(node)}
+                  component="tr"
+                  data-index={virtualRow.index}
+                  style={{
+                    transform: `translateY(${
+                      virtualRow.start - index * virtualRow.size
+                    }px)`,
+                  }}
+                >
+                  {row.getVisibleCells().map((cell) => {
+                    //   const meta: any = cell.column.columnDef.meta;
+                    return (
+                      <TableCell
+                        key={cell.id}
+                        component="td"
+                        style={{
+                          width: cell.column.getSize(),
+                        }}
+                      >
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    );
+                  })}
+                </TableRow>
+              );
+            })}
+          </TableBody>
+        </Table>
+      </div>
+    </TableContainer>
+  );
 }
--- packages/frontend/app/routes/_authenticated/admin_/index.tsx
+++ packages/frontend/app/routes/_authenticated/admin_/index.tsx
@@ -4,12 +4,12 @@ import DefaultAppBar from "@/components/DefaultAppBar";
 import { usersQueryOptions } from "@/queries/users.queries";
 
 import {
-	Button,
-	CircularProgress,
-	Container,
-	InputAdornment,
-	Stack,
-	TextField,
+  Button,
+  CircularProgress,
+  Container,
+  InputAdornment,
+  Stack,
+  TextField,
 } from "@mui/material";
 import { useSuspenseInfiniteQuery } from "@tanstack/react-query";
 import { createFileRoute, stripSearchParams } from "@tanstack/react-router";
@@ -23,126 +23,133 @@ import { useCallback } from "react";
 const columnHelper = createColumnHelper<z.infer<typeof UserSchema>>();
 
 const columns = [
-	columnHelper.accessor("username", {
-		header: "Username",
-	}),
-	columnHelper.display({
-		header: "Settings",
-		cell: (row) => (
-			<AsChildLink
-				to={"/admin/users/$userId"}
-				params={{
-					userId: row.row.original.id,
-				}}
-			>
-				<Button variant="outlined">Settings</Button>
-			</AsChildLink>
-		),
-	}),
+  columnHelper.accessor("username", {
+    header: "Username",
+  }),
+  columnHelper.display({
+    header: "Settings",
+    cell: (row) => (
+      <AsChildLink
+        to={"/admin/users/$userId"}
+        params={{
+          userId: row.row.original.id,
+        }}
+      >
+        <Button variant="outlined">Settings</Button>
+      </AsChildLink>
+    ),
+  }),
 ];
 
 const defaultValues = {
-	query: "",
-}
+  query: "",
+};
 
 const searchSchema = z.object({
-	query: z.string().default(defaultValues.query),
+  query: z.string().default(defaultValues.query),
 });
 
-
 export const Route = createFileRoute("/_authenticated/admin_/")({
-	validateSearch: searchSchema,
-	search: {
-		middlewares: [stripSearchParams(defaultValues)],
-	},
-	loaderDeps: ({ search }) => ({ search }),
-	loader: async ({ context: { queryClient }, deps: { search } }) =>
-		queryClient.prefetchInfiniteQuery(
-			usersQueryOptions.userList({
-				limit: 10,
-				search: search.query,
-			}),
-		),
-	head: () => ({
-		meta: [
-			{
-				title: "Admin - Users",
-			},
-		],
-	}),
-	component: Component,
+  validateSearch: searchSchema,
+  search: {
+    middlewares: [stripSearchParams(defaultValues)],
+  },
+  loaderDeps: ({ search }) => ({ search }),
+  loader: async ({ context: { queryClient }, deps: { search } }) =>
+    queryClient.prefetchInfiniteQuery(
+      usersQueryOptions.userList({
+        limit: 10,
+        search: search.query,
+      })
+    ),
+  head: () => ({
+    meta: [
+      {
+        title: "Admin - Users",
+      },
+    ],
+  }),
+  component: Component,
 });
 
 function Component() {
-	const { query } = Route.useSearch();
-	const navigate = Route.useNavigate();
+  return (
+    <>
+      <DefaultAppBar title="Admin - Users" />
+      <Container sx={{ my: 2, flex: 1, overflowY: "auto" }}>
+        <UserTable />
+      </Container>
+    </>
+  );
+}
 
-	const usersQuery = useSuspenseInfiniteQuery(
-		usersQueryOptions.userList({
-			limit: 10,
-			search: query,
-		}),
-	);
+function UserTable() {
+  const { query } = Route.useSearch();
+  const navigate = Route.useNavigate();
 
-	const pagedItems = useMemo(
-		() => usersQuery.data.pages.flatMap((page) => page.items) ?? [],
-		[usersQuery.data],
-	);
+  const usersQuery = useSuspenseInfiniteQuery(
+    usersQueryOptions.userList({
+      limit: 10,
+      search: query,
+    })
+  );
 
-	const total = useMemo(
-		() => usersQuery.data.pages.at(-1)?.total ?? 0,
-		[usersQuery.data],
-	);
+  const pagedItems = useMemo(
+    () => usersQuery.data.pages.flatMap((page) => page.items) ?? [],
+    [usersQuery.data]
+  );
 
-	const setSearch = useCallback(
-		(v: string) =>
-			navigate({
-				search: {
-					query: v,
-				},
-			}),
-		[navigate],
-	);
+  const total = useMemo(
+    () => usersQuery.data.pages.at(-1)?.total ?? 0,
+    [usersQuery.data]
+  );
 
-	return (
-		<>
-			<DefaultAppBar title="Admin - Users" />
-			<Container sx={{ my: 2, flex: 1, overflowY: "auto" }}>
-				<Stack gap={2} sx={{ height: "100%", display: "flex" }}>
-					<TextField
-						onChange={(e) => setSearch(e.target.value)}
-						defaultValue={query}
-						label="Search"
-						placeholder="Search for users"
-						fullWidth
-						slotProps={{
-							inputLabel: {
-								shrink: true,
-							},
-							input: {
-								endAdornment: usersQuery.isFetching ? (
-									<InputAdornment position="end">
-										<CircularProgress size={"30px"} />
-									</InputAdornment>
-								) : undefined,
-							},
-						}}
-					/>
-					<Datatable
-						columns={columns}
-						data={pagedItems}
-						globalFilter={query}
-						setGlobalFilter={setSearch}
-						fetchNextPage={usersQuery.fetchNextPage}
-						isFetching={usersQuery.isFetching}
-						totalDBRowCount={total}
-						fixedHeight={69.5}
-						sx={{
-							flex: 1,
-						}}
-					/>
-				</Stack>
-			</Container>
-		</>
-	);
+  const setSearch = useCallback(
+    (v: string) =>
+      navigate({
+        search: {
+          query: v,
+        },
+      }),
+    [navigate]
+  );
+
+  return (
+    <>
+      <Stack gap={2} sx={{ height: "100%", display: "flex" }}>
+        <TextField
+          onChange={(e) => setSearch(e.target.value)}
+          defaultValue={query}
+          label="Search"
+          placeholder="Search for users"
+          fullWidth
+          slotProps={{
+            inputLabel: {
+              shrink: true,
+            },
+            input: {
+              endAdornment: usersQuery.isFetching ? (
+                <InputAdornment position="end">
+                  <CircularProgress size={"30px"} />
+                </InputAdornment>
+              ) : undefined,
+            },
+          }}
+        />
+        <Datatable
+          columns={columns}
+          data={pagedItems}
+          globalFilter={query}
+          setGlobalFilter={setSearch}
+          fetchNextPage={usersQuery.fetchNextPage}
+          isFetching={usersQuery.isFetching}
+          totalDBRowCount={total}
+          fixedHeight={69.5}
+          sx={{
+            flex: 1,
+          }}
+        />
+      </Stack>
+    </>
+  );
 }
