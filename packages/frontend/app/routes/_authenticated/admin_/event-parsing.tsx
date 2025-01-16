35652bb450a6d9a2ce98b9df81328e2ab69bec18
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2025-01-17 10:51:10 +1100

    basic retreival of parsing rules

--- packages/frontend/app/routes/_authenticated/admin_/event-parsing.tsx
+++ packages/frontend/app/routes/_authenticated/admin_/event-parsing.tsx
@@ -1,16 +1,42 @@
 import DefaultAppBar from "@/components/DefaultAppBar";
-import { Container } from "@mui/material";
+import { adminQueries } from "@/queries/adminQueries";
+import { adminQueryKeys } from "@/server/queryKeys";
+import {
+  Container,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { MdInbox } from "react-icons/md";
 
 export const Route = createFileRoute("/_authenticated/admin_/event-parsing")({
+  loader: ({ context: { queryClient } }) => {
+    queryClient.prefetchQuery(adminQueries.eventParsingRules);
+  },
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const parsingRulesQuery = useSuspenseQuery(adminQueries.eventParsingRules);
+
   return (
     <>
       <DefaultAppBar title="Admin - Event Parsing" />
-      <Container sx={{ my: 2, flex: 1, overflowY: "auto" }}></Container>
+      <Container sx={{ my: 2, flex: 1, overflowY: "auto" }}>
+        <List>
+          {parsingRulesQuery.data.map((rule) => (
+            <ListItem disablePadding key={rule.id}>
+              <ListItemButton>
+                <ListItemText primary={rule.name} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Container>
     </>
   );
 }
