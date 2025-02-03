edf7525d5c220b058036c3459ab6ad0a97378bd9
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2025-02-03 14:37:24 +1100

    using type integer

--- packages/frontend/drizzle.config.ts
+++ packages/frontend/drizzle.config.ts
@@ -1,13 +1,14 @@
 import type { Config } from "drizzle-kit";
+import env from "./app/server/env.ts";
 
 export default {
   dialect: "postgresql",
   schema: "./app/server/drizzle/schema.ts",
   out: "./drizzle",
   dbCredentials: {
-    host: "localhost",
-    user: "postgres",
-    database: "postgres",
-    password: "postgres",
+    host: env.VITE_POSTGRES_HOST,
+    user: env.VITE_POSTGRES_USER,
+    database: env.VITE_POSTGRES_DB,
+    password: env.VITE_POSTGRES_PASSWORD,
   },
 } satisfies Config;
--- packages/frontend/drizzle/0021_greedy_rafael_vega.sql
+++ packages/frontend/drizzle/0021_greedy_rafael_vega.sql
@@ -1,3 +1,3 @@
 DROP INDEX "Event_secret_key";--> statement-breakpoint
-ALTER TABLE "EventParsingRule" ALTER COLUMN "kronosId" SET DATA TYPE integer;--> statement-breakpoint
+ALTER TABLE "EventParsingRule" ALTER COLUMN "kronosId" SET DATA TYPE integer USING "kronosId"::integer;--> statement-breakpoint
 ALTER TABLE "Event" ADD CONSTRAINT "Event_secret_key" UNIQUE("secret");
