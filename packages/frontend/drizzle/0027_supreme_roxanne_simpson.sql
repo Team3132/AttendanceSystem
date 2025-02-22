3456e49eb69bac443d8c48d6efe0398aadd0bd77
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2025-02-22 12:56:40 +1100

    update migration

--- packages/frontend/drizzle/0027_supreme_roxanne_simpson.sql
+++ packages/frontend/drizzle/0027_supreme_roxanne_simpson.sql
@@ -1,4 +1,5 @@
 ALTER TABLE "RSVP" ADD COLUMN "arrivingAt" timestamp (3) with time zone;--> statement-breakpoint
 ALTER TABLE "RSVP" ADD COLUMN "leavingAt" timestamp (3) with time zone;--> statement-breakpoint
+UPDATE "RSVP" r SET "arrivingAt" = CASE WHEN r."delay" IS NOT NULL THEN e."startDate" + (r."delay" || ' minutes')::interval ELSE NULL END FROM "Event" e WHERE r."eventId" = e."id";--> statement-breakpoint
 ALTER TABLE "RSVP" DROP COLUMN "delay";--> statement-breakpoint
 ALTER TABLE "User" DROP COLUMN "additionalOutreachHours";
