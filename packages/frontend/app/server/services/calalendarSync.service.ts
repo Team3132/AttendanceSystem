0ffed296584480eb47c1ead6768d1eadb8fd74e0
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2025-03-02 17:12:52 +1100

    set where columns

--- packages/frontend/app/server/services/calalendarSync.service.ts
+++ packages/frontend/app/server/services/calalendarSync.service.ts
@@ -1,7 +1,7 @@
 import { type calendar_v3, google } from "googleapis";
 
 import { trytm } from "@/utils/trytm";
-import { type SQL, asc, getTableColumns, inArray, sql } from "drizzle-orm";
+import { type SQL, asc, getTableColumns, inArray, or, sql } from "drizzle-orm";
 import type { PgTable } from "drizzle-orm/pg-core";
 import type { SQLiteTable } from "drizzle-orm/sqlite-core";
 import { DateTime } from "luxon";
@@ -208,10 +208,60 @@ const buildConflictUpdateColumns = <
 };
 
 /**
+ * Builds the set columns for the on conflict clause to check to see if the excluded column value is different from the current column value
+ *
+ * On conflict only updates the entry if any of the columns are different
+ *
+ * @param table Table to build the columns for
+ * @param columns Columns to set
+ * @returns SQL object with the columns to set based on exclusions
+ */
+const buildSetWhereColumns = <
+  T extends PgTable | SQLiteTable,
+  Q extends keyof T["_"]["columns"],
+>(
+  table: T,
+  columns: Q[],
+) => {
+  /** The table columns */
+  const cls = getTableColumns(table);
+
+  /** The statements to check if the column is different */
+  const statements = columns.map((column) => {
+    /** The reference to the column */
+    const col = cls[column];
+    /** The column name */
+    const colName = col.name;
+    /** The excluded column as a raw SQL object */
+    const excluded = sql.raw(`excluded.${colName}`);
+
+    // Check if the column is different
+    return sql`${col} != ${excluded}`;
+  });
+
+  // Return the or statement of all the statements
+  return or(...statements);
+};
+
+/**
  * Columns to update on conflict in the event table, everything that could change in a Google Calendar event
  * This excludes any changes made locally
  */
 const conflictUpdateColumns = buildConflictUpdateColumns(eventTable, [
+  "title",
+  "startDate",
+  "endDate",
+  "description",
+  "allDay",
+  "type",
+  "isSyncedEvent",
+  "ruleId",
+]);
+
+/**
+ * Columns to check for changes in the event table to determine if the event should be updated
+ */
+const conflictWhereColumns = buildSetWhereColumns(eventTable, [
   "title",
   "startDate",
   "endDate",
@@ -302,6 +352,7 @@ export const syncEvents = async () => {
         .onConflictDoUpdate({
           target: [eventTable.id],
           set: conflictUpdateColumns,
+          setWhere: conflictWhereColumns,
         })
         .returning({
           id: eventTable.id,
