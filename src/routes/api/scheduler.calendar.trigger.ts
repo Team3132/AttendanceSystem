7549009717a166885fe0383217220c2ce1ae273b
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2025-05-31 15:16:06 +1000

    fix again

--- src/routes/api/scheduler.calendar.trigger.ts
+++ src/routes/api/scheduler.calendar.trigger.ts
@@ -2,7 +2,9 @@ import { syncEvents } from "@/server/services/calalendarSync.service";
 import { json } from "@tanstack/react-start";
 import { createServerFileRoute } from "@tanstack/react-start/server";
 
-export const ServerRoute = createServerFileRoute().methods({
+export const ServerRoute = createServerFileRoute(
+  "/api/scheduler/calendar/trigger",
+).methods({
   POST: async () => {
     try {
       const result = await syncEvents();
