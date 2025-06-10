3a55841cc203cdd6ab5757df48b17335fbdb68fc
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2025-06-11 08:24:22 +1000

    any zod

--- src/hooks/useZodForm.ts
+++ src/hooks/useZodForm.ts
@@ -2,7 +2,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { type UseFormProps, useForm } from "react-hook-form";
 import type { z } from "zod";
 
-export default function useZodForm<TSchema extends z.ZodType>(
+export default function useZodForm<TSchema extends z.ZodAny>(
   props: Omit<UseFormProps<TSchema["_input"]>, "resolver"> & {
     schema: TSchema;
   },
