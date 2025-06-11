150adbf086ca83a0725fb71c48f5f66e38a0fd7b
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2025-06-11 10:04:56 +1000

    manual cache control

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,6 +1,7 @@
 export default defineNitroConfig({
   routeRules: {
     "/assets/**": {
+      static: true,
       headers: {
         "cache-control": `public, max-age=${365 * 24 * 60 * 60}, immutable`, // 1 year
       },
